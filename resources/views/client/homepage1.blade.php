3568d50c120cbbd38e4d184777e8a8c313c568dd
yoan446 <yoantioma4@gmail.com>
2025-10-11 20:43:04 +0100

    intégration du frontend partie visiteur responsive gérer  version 6

--- resources/views/client/homepage1.blade.php
+++ resources/views/client/homepage1.blade.php
@@ -7,7 +7,7 @@
     <!-- Lien vers le fichier CSS -->
     <link href="{{ asset('css/homepage1-style.css') }}" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>  
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
